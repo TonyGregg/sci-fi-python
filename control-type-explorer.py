4788a0dbb91446e5cb6eca1b8f81d62270e1995a
Antony G <antony.gregory@shoes.com>
2021-08-22 17:51:01 -0500

    explored if-else

---
+++ control-type-explorer.py
@@ -0,0 +1,17 @@
+def explore_if_else():
+    x = 8
+    if x > 8:
+        y = 20
+        z = 100
+    elif x < 8:
+        y = -25
+        z = -125
+    else:
+        w = 200;
+        y = 0;
+        z = 0;
+        print(x, y, z, w)
+
+
+if __name__ == '__main__':
+    explore_if_else()
